855b83a96ebd2cc2be5dd2382ba00facf4119c6c
akashbhadani02 <akashbhadani5050@gmail.com>
2026-08-21 22:27:28 +0530

    update

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'lead-followup-manager-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './img/icon-192.png', './img/icon-512.png', './img/tectrum.jpeg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
