06e386260c5291fcfe3d4d7db3e0114f180aaab9
akashbhadani02 <akashbhadani5050@gmail.com>
2026-08-22 07:55:24 +0530

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lead-manager-v2";
+const CACHE = "lead-manager-v3";
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", event => {
