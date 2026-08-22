9907405ecfb92f171410514022ed7714e5ad1c49
akashbhadani02 <akashbhadani5050@gmail.com>
2026-08-22 07:59:18 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'lead-followup-manager-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './img/icon-192.png', './img/icon-512.png', './img/tectrum.jpeg'];
+const CACHE = 'lead-followup-manager-v2';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.ico', './img/favicon.png', './img/icon-192.png', './img/icon-512.png', './img/tectrum.jpeg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lead-manager-v3";
+const CACHE = "lead-manager-v4";
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", event => {
